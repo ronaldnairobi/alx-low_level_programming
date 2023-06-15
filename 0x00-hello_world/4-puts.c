e1f3febf67a9ee74c8d132a37d01367880e10220
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-15 11:36:31 -0700

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,16 +3,15 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints the message "Programming is like building a multilingual puzzle"
- *              using the puts function. Returns 0 to indicate successful execution.
+ * Description:
+ *     Prints the message "Programming is like building a multilingual puzzle"
+ *     using the puts function. Returns 0 to indicate successful execution.
  *
  * Return: Always 0
  */
 int main(void)
 {
-	/* Print the message using the puts function */
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+	return 0;
 }
 
