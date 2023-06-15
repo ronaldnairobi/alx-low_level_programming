064258a9f286902d770f6c7fa9ed99774be98afe
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-15 05:19:51 -0700

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,17 @@
-/*
- * File: puzzle.c
- * Author: Your Name
- * Description: A program that prints the message "Programming is like building a multilingual puzzle"
- *              using the puts function, following the Betty coding and documentation style.
- */
-
 #include <stdio.h>
 
 /**
  * main - Entry point of the program
  *
+ * Description: Prints the message "Programming is like building a multilingual puzzle"
+ *              followed by a new line.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *message = "Programming is like building a multilingual puzzle";
+    puts "Programming is like building a multilingual puzzle" ;
 
-	puts(message);
-
-	return (0);
+    return (0);
 }
 
