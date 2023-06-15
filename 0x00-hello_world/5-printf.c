b971e208c07ca7818967418513d0835a7ad77e62
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-15 05:23:04 -0700

    new file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    printf("         ,/|");
+    printf("\n");
+    printf("       ,// |");
+    printf("\n");
+    printf("     ,//   |");
+    printf("\n");
+    printf("   ,//_____\\____");
+    printf("\n");
+    printf(" ==_____(__6_\\______");
+    printf("\n");
+    printf("           \"  \"");
+    printf("\n");
+
+    return 0;
+}
+
