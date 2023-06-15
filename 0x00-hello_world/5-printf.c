18ea843696c3ff969dd7c4fafc0054ff34ddf198
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-15 05:42:30 -0700

    new file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a piece of art with proper grammar,
+ *              followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("         ,/|\n");
+	printf("       ,// |\n");
+	printf("     ,//   |\n");
+	printf("   ,//_____\\____\n");
+	printf(" ==_____(__6_\\______\n");
+	printf("           \"  \"\n");
+
+	return (0);
+}
+
