c00b359db23ba0971eda30ff0a81870a92990f46
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-16 12:46:25 -0700

    new file 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * determineSign - Determines if a number is positive, negative, or zero
+ * @number: The number to check
+ *
+ * Return: A string indicating the sign of the number
+ */
+const char *determineSign(int number)
+{
+	if (number > 0)
+		return "is positive";
+	else if (number == 0)
+		return "is zero";
+	else
+		return "is negative";
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	srand(time(0)); /* Seed the random number generator with current time */
+
+	int n = rand() - RAND_MAX / 2;
+
+	printf("The number %d %s\n", n, determineSign(n));
+
+	return 0;
+}
+
