87478b7b5216d9947dd8b5837ed0bd6db64e6376
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-16 15:17:29 -0700

    modified

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,15 +9,16 @@
  */
 int main(void)
 {
-    int digit;
+int digit;
 
-    for (digit = 0; digit <= 9; digit++)
-    {
-        putchar(digit + '0');
-    }
+for (digit = 0; digit <= 9; digit++)
+{ 
+putchar(digit + '0');
+}
 
-    putchar('\n');
-
-    return 0;
+putchar('\n');
+{
+return 0;
+}
 }
 
