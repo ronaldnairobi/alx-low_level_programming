658045a37087df3e8087b400684c3d247b82b333
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-16 13:08:37 -0700

    new file 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,24 +9,25 @@
  */
 int main(void)
 {
+	int n;
+	int lastDigit;
+
 	srand(time(0)); /* Seed the random number generator with current time */
 
-	int n = rand() - RAND_MAX / 2;
-	int lastDigit = abs(n % 10); /* Get the absolute value of the last digit of n */
-
-	printf("The string Last digit of %d is ", n);
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
 
 	if (lastDigit > 5)
 	{
-		printf("%d and is greater than 5\n", lastDigit);
+		printf("The last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	}
 	else if (lastDigit == 0)
 	{
-		printf("%d and is 0\n", lastDigit);
+		printf("The last digit of %d is %d and is 0\n", n, lastDigit);
 	}
 	else
 	{
-		printf("%d and is less than 6 and not 0\n", lastDigit);
+		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
 
 	return 0;
