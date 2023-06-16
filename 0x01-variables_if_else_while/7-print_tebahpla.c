f671c2eecf1668da5572a96c4d0bde0377b5dbb7
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-16 14:05:34 -0700

    new file 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 122; letter >= 97; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
