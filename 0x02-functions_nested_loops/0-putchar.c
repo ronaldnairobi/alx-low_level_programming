280e83b74e2d5fb3f5b3056b5d25ee86e03016b9
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-20 11:21:45 -0700

    modified

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
@@ -8,7 +8,7 @@
  */
 int _putchar(char c)
 {
-	return putchar(c);
+	return _putchar(c);
 }
 
 /**
