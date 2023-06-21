24bc7a36a59002cd4c9377f26e0d6edf3fe8862f
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-20 23:53:57 -0700

    new file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,23 +12,3 @@ int _putchar(char c)
 	return write(1, &c, 1);
 }
 
-/**
- * main - entry point of the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
-}
-
