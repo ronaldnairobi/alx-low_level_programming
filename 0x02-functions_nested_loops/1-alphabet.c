ac983ebf53590a53853d22972be9ae82cc225c6b
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-21 00:23:38 -0700

    modified

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+}
+
