e58f9c38b4618806e1213484e80f8ff826c8d06e
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-21 05:20:03 -0700

    modified

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -2,21 +2,29 @@
 #include "main.h"
 
 /**
+ * test_islower - Test the _islower function.
+ */
+void test_islower(void)
+{
+    int r;
+
+    r = _islower('H');
+    putchar(r + '0');
+    r = _islower('o');
+    putchar(r + '0');
+    r = _islower(108);
+    putchar(r + '0');
+    putchar('\n');
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0
  */
 int main(void)
 {
-    int r;
-
-    r = _islower('H');
-     putchar(r + '0');
-    r = _islower('o');
-     putchar(r + '0');
-    r = _islower(108);
-     putchar(r + '0');
-     putchar('\n');
+    test_islower();
     return 0;
 }
 
