3d6261e9a63f83ad28e8f495cb4e66158ad1a1d5
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-22 13:13:54 -0700

    modified

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,27 +1,17 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - writes a character to the standard output
- * @c: the character to be written
- *
- * Return: on success, the character written; on error, -1
+ * print_numbers - print from 0 to 9
+ * Return: always 0
  */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
 
-/**
- * print_numbers - prints numbers from 0 to 9 followed by a new line
- */
 void print_numbers(void)
 {
-    int i;
+char c;
 
-    for (i = 0; i <= 9; i++)
-    {
-        _putchar(i + '0');
-    }
-    _putchar('\n');
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+putchar('\n');
 }
