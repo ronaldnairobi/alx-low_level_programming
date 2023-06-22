3cafaee30d1281bbab0b885f76b4fb7ea2ed2b72
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-22 13:53:35 -0700

    modified

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,35 +1,19 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes a character to the standard output
- * @c: the character to be written
- *
- * Return: on success, the character written; on error, -1
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
 
 /**
  * print_line - draws a straight line in the terminal
  * @n: the number of times the character '_' should be printed
+ * Return: always 0
  */
+
 void print_line(int n)
 {
-    int i;
+int i;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+for (i = 0; i < n; i++)
+{
+putchar('_');
+}
 
-    for (i = 0; i < n; i++)
-    {
-        _putchar('_');
-    }
-
-    _putchar('\n');
+putchar('\n');
 }
