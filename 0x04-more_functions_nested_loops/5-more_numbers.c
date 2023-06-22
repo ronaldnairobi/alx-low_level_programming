c03a5c55a86161a291118de905f559a8316a47cf
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-22 15:07:31 -0700

    modified

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,19 @@ void more_numbers(void)
 {
 int i, j;
 
-for (i = 1; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
-for (j = 0; j <= 14; j++)
+for (j = 0; j < 15; j++)
 {
-if (j >= 10)
+if (j < 10 || j > 14)
 {
-putchar('1');
-}
 putchar(j % 10 + '0');
 }
+else
+{
+putchar(j / 10 + '0');
+}
 putchar('\n');
+}
 }
 }
