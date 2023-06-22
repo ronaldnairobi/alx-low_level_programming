ef7959cd6bddaca3b48c7d5af9369aef5c0d341f
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-22 13:27:29 -0700

    new file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,28 +1,18 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - writes a character to the standard output
- * @c: the character to be written
- *
- * Return: on success, the character written; on error, -1
+ * print_most_numbers - prints 01356789
+ * Return:void
  */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
 
-/**
- * print_most_numbers - prints numbers from 0 to 9 excluding 2 and 4
- */
 void print_most_numbers(void)
 {
-    int i;
+char c;
 
-    for (i = 0; i <= 9; i++)
-    {
-        if (i != 2 && i != 4)
-            _putchar(i + '0');
-    }
-    _putchar('\n');
+for (c = 0; c <= 9; c++)
+{
+if (!(c == '2' && c == '4'))
+putchar(c);
+}
+putchar('\n');
 }
