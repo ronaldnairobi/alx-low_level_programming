a02ba45f481a2f309631da6153aca022866c1781
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-22 13:42:22 -0700

    new file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,34 +1,23 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes a character to the standard output
- * @c: the character to be written
- *
- * Return: on success, the character written; on error, -1
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
 
 /**
  * more_numbers - prints numbers from 0 to 14, ten times
+ * Return: void
  */
 void more_numbers(void)
 {
-    int i, j;
+int i, j;
 
-    for (j = 0; j < 10; j++)
-    {
-        for (i = 0; i <= 14; i++)
-        {
-            if (i > 9)
-            {
-                _putchar('1');
-            }
-            _putchar(i % 10 + '0');
-        }
-        _putchar('\n');
-    }
+for (i = 1; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+{
+putchar('1');
+}
+putchar(j % 10 + '0');
+}
+putchar('\n');
+}
 }
