653fb6851a9dd6b43f7ad0dc07356ca5c236b2cd
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-22 14:05:10 -0700

    modified

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,37 +1,26 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes a character to the standard output
- * @c: the character to be written
- *
- * Return: on success, the character written; on error, -1
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
 
 /**
  * print_square - prints a square in the terminal
  * @size: the size of the square
+ * Return: void
  */
+
 void print_square(int size)
 {
-    int i, j;
+int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (size <= 0)
+{
+putchar('\n');
+}
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+putchar('#');
+}
+putchar('\n');
+}
 }
