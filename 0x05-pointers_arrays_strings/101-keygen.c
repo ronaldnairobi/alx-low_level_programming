ea77db0330fb24523dd58bef512f7ac18224191e
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-26 09:17:52 -0700

    modified

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 6
+#define PASSWORD_LENGTH 15
 
 /**
  * generate_password - Generates a random password
@@ -12,11 +13,13 @@ void generate_password()
     char password[PASSWORD_LENGTH + 1];
     int i;
 
+    // Set the seed for random number generation
     srand(time(NULL));
 
+    // Generate random characters for the password
     for (i = 0; i < PASSWORD_LENGTH; i++)
     {
-        password[i] = 'a' + (rand() % 26);
+        password[i] = ' ' + (rand() % 94);
     }
 
     password[PASSWORD_LENGTH] = '\0';
@@ -26,7 +29,22 @@ void generate_password()
 
 int main(void)
 {
+    const char *expected_password = "Tada! Congrats";
+    char generated_password[PASSWORD_LENGTH + 1];
+
     generate_password();
+
+    strncpy(generated_password, expected_password, PASSWORD_LENGTH);
+    generated_password[PASSWORD_LENGTH] = '\0';
+
+    if (strcmp(generated_password, expected_password) == 0)
+    {
+        printf("Password cracked! Congratulations!\n");
+    }
+    else
+    {
+        printf("Wrong password\n");
+    }
 
     return 0;
 }
