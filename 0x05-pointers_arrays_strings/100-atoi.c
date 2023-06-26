e3cc61c523af0995f1f1bb3cff5ce1784ab43b36
ronaldnairobi <ronaldjnr3@gmail.com>
2023-06-26 09:06:15 -0700

    modified

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,35 +11,26 @@
 int _atoi(char *s)
 {
 	int sign = 1;
-	unsigned int result = 0;
+	int result = 0;
 	int i = 0;
-
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
 
 	while (s[i])
 	{
+		if (s[i] == '-')
+			sign *= -1;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (result <= (UINT_MAX / 10))
-				result = result * 10 + (s[i] - '0');
+			if (result == 0)
+				result = s[i] - '0';
 			else
-				break;
+				result = result * 10 + (s[i] - '0');
 		}
-		else
+		else if (result != 0)
 		{
 			break;
 		}
 		i++;
 	}
-
-	if (result > (UINT_MAX / 10) && sign == -1)
-		return (INT_MIN);
-	else if (result > INT_MAX && sign == 1)
-		return (INT_MAX);
 
 	return (result * sign);
 }
