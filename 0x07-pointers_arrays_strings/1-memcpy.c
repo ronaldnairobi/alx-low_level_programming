1bc5373a93df5a82738d9719bed883b74aaa3c65
ronaldnairobi <ronaldjnr3@gmail.com>
2023-07-04 03:02:57 -0700

    new files

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memcpy - entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: success always 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
+}
