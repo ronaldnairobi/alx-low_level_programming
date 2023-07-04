673afb66c4cd02039159fa57ab0f5abef3c20e04
ronaldnairobi <ronaldjnr3@gmail.com>
2023-07-04 04:28:30 -0700

    new files

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - entry point
+ * @haystack: input
+ * @needle: input
+ * Return: succes always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *one = haystack;
+char *two = needle;
+while (*one == *two && *two != '\0')
+{
+one++;
+two++;
+}
+if (*two == '\0')
+return (haystack);
+}
+return (NULL);
+}
