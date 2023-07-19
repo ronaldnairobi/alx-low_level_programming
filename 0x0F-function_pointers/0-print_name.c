52d6a42388011f919e3cae1fe5a2f389826602c2
ronaldnairobi <ronaldjnr3@gmail.com>
2023-07-19 02:58:53 -0700

    new files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: pointer to function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+f(name);
+}
