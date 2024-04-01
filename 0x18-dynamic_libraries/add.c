1af175ebf97047f1c84e943cbe8a1d613e0a0f65
ronaldnairobi <ronaldjnr3@gmail.com>
2024-03-31 22:57:25 -0700

    new files

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - adds two integers
+ * @a: first int
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}
